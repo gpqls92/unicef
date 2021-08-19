e36b1d8a15a69c29461692b2137f6292a148d750
gpqls92 <86402466+gpqls92@users.noreply.github.com>
2021-08-19 14:55:27 +0900

    update3

--- js/support_4.js
+++ js/support_4.js
@@ -16,21 +16,38 @@ window.addEventListener('DOMContentLoaded',function(){
             join.action = '1_drough.html';
             join.submit();
         }
-
-        // $('.yes li').on('click',function(){
-        //     if($(this).hasClass('active')){
-        //     $('.yes li').removeClass('active').find('div').stop().slideUp();
-        //     return;
-        // }
-        // $('.yes li').removeClass('active').find('div').slideUp();
-        // $(this).addClass('active').find('div').stop().slideDown();
-        // });
-
-
+    // 약관 및 개인정보 동의 상세보기 내용
+        $('.yes li').on('click',function(){
+            if($(this).hasClass('active')){
+            $('.yes li').removeClass('active').find('div').stop().slideUp();
+            return;
+        }
+        $('.yes li').removeClass('active').find('div').slideUp();
+        $(this).addClass('active').find('div').stop().slideDown();
+        });
 
 
 
+    //주민번호체크(per2)
+    const elInput = document.querySelector('.per2 input');
+    const elBtn = document.querySelector('.per2 button');
 
+    elBtn.addEventListener('click',function(){
+        let num = elInput.value,
+            key = [2,3,4,5,6,7,8,9,2,3,4,5],
+            total = 0;
+
+        for(let i in key){
+            total += key[i] * num[i];
+        }
+        
+        let checkNum = Math.floor(  (11-(total%11))%10  );
+        if(num[12] == checkNum){
+            alert('정상')
+        }else{
+            alert('비정상')
+        }        
+    });
 
 
 
